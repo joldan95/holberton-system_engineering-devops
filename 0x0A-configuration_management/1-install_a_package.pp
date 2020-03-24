87b98b22d006735fc8c779e95b66d224f71a766f
luismedinaeng <malf95@hotmail.com>
2020-03-23 20:31:20 -0500

    [Update]Add first line of comments

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,4 @@
+# Install the puppet-lint package
 package { 'puppet-lint':
   ensure   => '2.1.1',
   provider => 'gem',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# Kill a proccess
 exec { 'pkill killmenow' :
   command => '/usr/bin/pkill killmenow'
 }
