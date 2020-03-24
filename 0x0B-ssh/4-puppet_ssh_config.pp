f4083cfcde8cf7ec391b254ab223e28c439c2081
luismedinaeng <malf95@hotmail.com>
2020-03-24 18:37:37 -0500

    [Add]Complete Task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Change the configuration file of ssh
+
+# Change password authentication
+file_line { 'ssh_config no pass':
+  ensure   => 'present',
+  path     => '/etc/ssh/ssh_config',
+  line     => '#   PasswordAuthentication yes',
+  match    => '#   PasswordAuthentication'
+}
+
+# Adds
+file_line { 'ssh_config add key':
+  ensure   => 'present',
+  path     => '/etc/ssh/ssh_config',
+  line     => '#   IdentityFile ~/.ssh/holberton',
+  match    => '#   IdentityFile ~/.ssh/holberton'
+}
