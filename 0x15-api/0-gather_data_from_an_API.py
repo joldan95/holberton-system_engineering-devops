335f55cf4de5753d5a77a0a459f51b20c9437def
luismedinaeng <malf95@hotmail.com>
2020-05-04 20:22:46 -0500

    [Update]Change file permissions and [Fix]Style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,14 +8,15 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/users/{}' 
+    url = 'https://jsonplaceholder.typicode.com/users/{}'
     user_id = sys.argv[1]
     user = requests.get(url.format(user_id))
     if user.status_code == 200:
-        todo_list = requests.get(url.format(user_id) + '/todos').json()
-        complete = list(filter(lambda todo: todo['completed'] == True, todo_list))
+        todos = requests.get(url.format(user_id) + '/todos').json()
+        complete = list(filter(lambda todo: todo['completed'] is True, todos))
         complete = list(task['title'] for task in complete)
-        print("Employee {} is done with tasks({:d}/{:d}):".format(user.json()['name'],
-                                                                  len(complete),
-                                                                  len(todo_list)),
+        first_line = "Employee {} is done with tasks({:d}/{:d}):"
+        print(first_line.format(user.json()['name'],
+                                len(complete),
+                                len(todos)),
               *complete, sep='\n\t ')
