940cf4ee0ca1038ca5db0be60498bb51841be120
luismedinaeng <malf95@hotmail.com>
2020-05-04 20:57:13 -0500

    [Add]Complete Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Export user data to a CSV format
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/{}'
+    user_id = sys.argv[1]
+    user = requests.get(url.format(user_id))
+    if user.status_code == 200:
+        todos = requests.get(url.format(user_id) + '/todos').json()
+        with open("{}.csv".format(user_id), "w") as user_file:
+            for task in todos:
+                s = "\"{}\",\"{}\",\"{}\",\"{}\"".format(user_id,
+                                                         user.json()["name"],
+                                                         task["completed"],
+                                                         task["title"])
+                print(s, file=user_file)
